97e85caeda2203ac1a2dcf8c2752061e3e3490de
S_quail <344913689@qq.com>
2019-01-28 10:55:28 +0800

    添加编辑，删除功能

--- app.py
+++ app.py
@@ -90,21 +90,28 @@ def index():
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
-
+    print(request.method)
     if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
+        title = request.form.get('title')
+        year = request.form.get('year')
 
-        if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('输入错误!')
+        if not title or not year or len(title) > 60 or len(year) > 4:
+            flash('输入错误，请重新输入！')
             return redirect(url_for('edit', movie_id = movie_id))
 
         movie.title = title
         movie.year = year
         db.session.commit()
-        flash('更新成功')
+        flash('更新成功！')
         return redirect(url_for('index'))
 
     return render_template('edit.html', movie = movie)
 
-    
+@app.route('/movie/delete/<int:movie_id>', methods=['GET', 'POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除完成！')
+    return redirect(url_for('index'))
+
